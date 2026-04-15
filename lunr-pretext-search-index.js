41b5d9e15cb16600cd303e55602fd213f46cd0ff
DuffRBJ <33201272+DuffRBJ@users.noreply.github.com>
2026-04-15 16:35:09 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,13 +1,31 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "shorttitlelowercase",
+  "id": "root-1-1-2",
   "level": "1",
-  "url": "#shorttitlelowercase",
-  "type": "Article",
-  "number": "",
-  "title": "The title of my groundbreaking paper",
-  "body": " The title of my groundbreaking paper    Find the eigenvalues and eigenspaces for the following matrix.     "
+  "url": "root-1-1-2.html",
+  "type": "[section]",
+  "number": "1",
+  "title": "7.0 — Review",
+  "body": " 7.0 — Review  A prerequisite for applying a strategy is a knowledge of the basic integration formulas. In the following table we have collected the integrals from our previous list together with several additional formulas that we have learned in this chapter. Most of them should be memorized.   Table of Integration Formulas                                     Constants of integration are omitted.  "
+},
+{
+  "id": "root-1-1-2-3",
+  "level": "2",
+  "url": "root-1-1-2.html#root-1-1-2-3",
+  "type": "[table]",
+  "number": "1.1",
+  "title": "",
+  "body": " Table of Integration Formulas                                    "
+},
+{
+  "id": "root-1-1-2-4",
+  "level": "2",
+  "url": "root-1-1-2.html#root-1-1-2-4",
+  "type": "[remark]",
+  "number": "1.2",
+  "title": "",
+  "body": "Constants of integration are omitted. "
 }
 ]
 
